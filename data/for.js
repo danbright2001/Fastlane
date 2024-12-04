610e40329fc999513f45b7f77256de5e5746f7fb
danbright2001 <124036628+danbright2001@users.noreply.github.com>
2024-12-04 18:06:11 +0300

    all new ones added

---
+++ data/for.js
@@ -0,0 +1,17 @@
+const forDetails = [{
+    icon: "beginners_icon.png",
+    title: "BEGINNERS",
+    text: ""
+},{
+    icon: "professional_icon.png",
+    title: "PROFESSIONALS",
+    text: "" 
+},{
+    icon: "entrepreneurs_icon.png",
+    title: "ENTREPRENEURS",
+    text: ""
+},{
+    icon: "business_owners_icon.png",
+    title: "BUSINESS OWNERS",
+    text: ""
+}];
