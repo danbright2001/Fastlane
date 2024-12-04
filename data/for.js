74d46ff6faebab8bb7ca23de382c1b9930373950
danbright2001 <124036628+danbright2001@users.noreply.github.com>
2024-12-04 23:38:20 +0300

    c

--- data/for.js
+++ data/for.js
@@ -4,15 +4,18 @@ export const forDetails = [{
     icon: "beginner_icon.png",
     title: "BEGINNERS",
     text: ""
-},{
+},
+{
     icon: "professional_icon.png",
     title: "PROFESSIONALS",
     text: "" 
-},{
+},
+{
     icon: "entrepreneurs_icon.png",
     title: "ENTREPRENEURS",
     text: ""
-},{
+},
+{
     icon: "business_owners_icon.png",
     title: "BUSINESS OWNERS",
     text: ""
