155b53822ec2b27a8eefc96b9aa36bc31955eb3b
danbright2001 <124036628+danbright2001@users.noreply.github.com>
2024-12-05 12:48:11 +0300

    for txt

--- data/for.js
+++ data/for.js
@@ -3,20 +3,20 @@
 export const forDetails = [{
     icon: "beginner_icon.png",
     title: "BEGINNERS",
-    text: ""
+    text: "Start Earning with Ease – No experience? No problem! Begin with simple jobs like transcription and data entry, and grow your skills with our supportive community."
 },
 {
     icon: "professional_icon.png",
     title: "PROFESSIONALS",
-    text: "" 
+    text: "Unlock High-Paying Gigs – If you’re an expert, find specialized, high-paying freelance opportunities in copywriting, graphic design, and more, and work with clients who value your experience." 
 },
 {
     icon: "entrepreneurs_icon.png",
     title: "ENTREPRENEURS",
-    text: ""
+    text: "Turn Ideas into Income – Use NileUp to generate income with creative online jobs, from content creation to e-commerce, while growing your business."
 },
 {
     icon: "business_owners_icon.png",
     title: "BUSINESS OWNERS",
-    text: ""
+    text: "Expand Your Skills – Take on specialized projects to enhance your skills and improve your business operations, whether it's marketing, content, or tech."
 }];
