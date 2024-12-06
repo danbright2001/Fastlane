9c69ee396f7b32b9d123c930b0786dad432496c0
danbright2001 <124036628+danbright2001@users.noreply.github.com>
2024-12-06 08:14:20 +0300

    one

--- data/testimonials.js
+++ data/testimonials.js
@@ -1,38 +1,38 @@
 export const testimonials = [
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Daniel Wekesa",
+    testimonial_position: "Software Engineer",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Ezra Makhulo",
+    testimonial_position: "Accountant",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Lincoln Karanja",
+    testimonial_position: "Finance analyst",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Peter Wekesa",
+    testimonial_position: "CEO CyberDen",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Alice Mulinge",
+    testimonial_position: "Student ANU",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 {
-    testimonial: "",
     testimonial_icon: "",
-    testimonial_name: "",
-    testimonial_position: ""
+    testimonial_name: "Ryian Kipngetich",
+    testimonial_position: "Attorney",
+    testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorem voluptas aspernatur autem, natus veritatis voluptate? Minima quam autem ipsam.",
 },
 ]
